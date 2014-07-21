c169fbccf4984f847cd93c45dfa2feef7ae3034b
alessandroRastelli <rastarm@hotmail.it>
2014-07-21 16:16:15 +0200

    final_project

---
+++ final_project/Readme.md
@@ -0,0 +1,48 @@
+**COMPUTATIONAL GRAPHICS - FINAL PROJECT**
+================
+Rastelli Alessandro - 418531
+-----------------------
+
+View the project [here] (http://alessandroRastelli.github.io).
+
+Code organization
+================
+* [windowAndDoor.js] (scripts/windowAndDoor.js): contains functions to create doors and windows.
+* [fpsAnimation.js] (scripts/fpsAnimation.js): is the code used to switch between trackball system and PointerLock.
+* [Furniture.js] (scripts/Furniture.js): used to import all of the .obj and .mtl files added in the scene.
+* [init.js] (scripts/init.js): initializes all the variables,import larcc model and creates the dwelling and takes care of the rendering.
+* [light.js] (scripts/light.js): contains functions to create lamps and lighting system.
+* [gui.js] (scripts/gui.js): contain graphical user interface of system.
+* [Animations.js] (scripts/Animations.js): contains all the animation assignement functions.
+* [ParticlesEffects.js] (scripts/ParticlesEffects.js): creates effects using particle system.
+* [textureUtility.js] (scripts/textureUtility.js): contains functions for the creation of textured meshes.
+* [wallFloor.js] (scripts/wallFloor.js): is where all the textured planes are created.
+* [objectAnimation.js] (scripts/wallFloor.js): contains functions to animate some specific objects
+
+Features and Graphic Techniques:
+==================
+* Bump map and normal map
+* Textures
+* Tween animations
+* .obj and .obj/.mtl models import
+* Skybox
+* PointerLockControl
+* FPSControl
+* Object-Picking
+* Particle System
+* Dynamic video texture
+* WebRTC WebCam
+* Audio distance fade
+* Sound effects
+
+Animations:
+==================
+* Doors and handles: open/close (with sound effect)
+* Windows and handles: open/close
+* Television: turn it on/off and change tv channel (with sound fade)
+* Cooker: light the fires
+* Toilet flushing button: push it and stare at the flush (with sound effect)
+* Computer monitor: Windows XP startup + webcam (with sound fade)
+* Light switches: switch up/down and make lamps light on/off
+* Car : drive the car (with sound effect)
+* AndroidGame: plays with the Android characters (with sound effect)
---
+++ final_project/scripts/fpsAnimation.js
@@ -0,0 +1,60 @@
+var controls;
+var havePointerLock = 'pointerLockElement' in document || 'mozPointerLockElement' in document || 'webkitPointerLockElement' in document;
+
+if (havePointerLock) {
+
+  var element = document.body;
+
+  var pointerlockchange = function(event) {
+    if (document.pointerLockElement === element || document.mozPointerLockElement === element || document.webkitPointerLockElement === element) {
+      FPenabled = true;
+      controls.enabled = true;
+     camera.position.set(0,0,0);
+      camera.lookAt(scene.position);
+
+      camera.up = new THREE.Vector3(0, 0, 0);
+      if(ModeGame===true){
+          $("#point").fadeIn(1000);
+      }else{
+          $("#pointer").fadeIn(1000);
+      }
+    } else {
+      location.reload();
+    }
+  }
+
+  var pointerlockerror = function(event) {
+    location.reload();
+  }
+
+  document.addEventListener('pointerlockchange', pointerlockchange, false);
+  document.addEventListener('mozpointerlockchange', pointerlockchange, false);
+  document.addEventListener('webkitpointerlockchange', pointerlockchange, false);
+
+  document.addEventListener('pointerlockerror', pointerlockerror, false);
+  document.addEventListener('mozpointerlockerror', pointerlockerror, false);
+  document.addEventListener('webkitpointerlockerror', pointerlockerror, false);
+
+  var startFPS = function() {
+    trackballControls.reset();
+    controls = new THREE.PointerLockControls(camera);
+    scene.add(controls.getObject());
+    element.requestPointerLock = element.requestPointerLock || element.mozRequestPointerLock || element.webkitRequestPointerLock;
+    if (/Firefox/i.test(navigator.userAgent)) {
+      var fullscreenchange = function(event) {
+        if (document.fullscreenElement === element || document.mozFullscreenElement === element || document.mozFullScreenElement === element) {
+          document.removeEventListener('fullscreenchange', fullscreenchange);
+          document.removeEventListener('mozfullscreenchange', fullscreenchange);
+          element.requestPointerLock();
+        }
+      }
+      document.addEventListener('fullscreenchange', fullscreenchange, false);
+      document.addEventListener('mozfullscreenchange', fullscreenchange, false);
+      element.requestFullscreen = element.requestFullscreen || element.mozRequestFullscreen || element.mozRequestFullScreen || element.webkitRequestFullscreen;
+      element.requestFullscreen();
+    } else {
+      element.requestPointerLock();
+    }
+  };
+
+}
